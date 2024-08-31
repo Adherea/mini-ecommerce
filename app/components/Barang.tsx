a42fe5cf60bba21e0968773604b500bedbacf930
DityaSismantoro <dityasismantoro@gmail.com>
2024-08-31 19:27:15 +0800

    commit pertama

--- app/components/Barang.tsx
+++ app/components/Barang.tsx
@@ -2,13 +2,28 @@
 import Image from "next/image";
 import React from "react";
 
-export default function Barang({ id, gambar, kategori, nama, harga }) {
+// Definisikan tipe data untuk properti yang diterima oleh komponen Barang
+interface BarangProps {
+  id: string;
+  gambar: string;
+  kategori: string;
+  nama: string;
+  harga: number;
+}
+
+export default function Barang({ id, gambar, kategori, nama, harga }: BarangProps) {
   return (
     <section className="rounded-md shadow-lg w-fit">
       <div className="relative overflow-hidden" style={{ width: "250px", height: "250px" }}>
         <Image
-          className={`transition-opacity duration-300 ease-in-out  rounded-tl-xl rounded-tr-xl `}
-          style={{ transition: "opacity 0.3s ease-in-out", objectFit: "cover", objectPosition: "center", width: "100%", height: "100%" }}
+          className={`transition-opacity duration-300 ease-in-out rounded-tl-xl rounded-tr-xl`}
+          style={{
+            transition: "opacity 0.3s ease-in-out",
+            objectFit: "cover",
+            objectPosition: "center",
+            width: "100%",
+            height: "100%",
+          }}
           src={gambar}
           width={950}
           height={250}
@@ -17,7 +32,7 @@ export default function Barang({ id, gambar, kategori, nama, harga }) {
       </div>
       <div className="px-5 py-3 rounded-bl-xl rounded-br-xl">
         <h1 className="pt-5">{nama}</h1>
-        <p className="py-2 ">Kategori: {kategori}</p>
+        <p className="py-2">Kategori: {kategori}</p>
         <h5 className="font-bold">Rp {harga}</h5>
       </div>
     </section>
